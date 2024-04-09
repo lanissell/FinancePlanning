237ac8e951c42e1d6da3151c719c7e491e5692a0
Denis Belavin <denis.belavin@saritasa.com>
2024-04-09 17:24:17 +0700

    Fix tests

--- Test/GoalServiceTest.py
+++ Test/GoalServiceTest.py
@@ -1,35 +1,40 @@
+import os
 import unittest
 from datetime import datetime
 
 from FinancePlanning.Logic.GoalService import GoalService
 from FinancePlanning.Logic.RevenuesService import RevenuesService
 from FinancePlanning.Models.Earning import Earning
-from FinancePlanning.Models.EarningSource import EarningSource
+from FinancePlanning.Models.Goal import Goal
+from FinancePlanning.Models.Revenue import Revenue
+from FinancePlanning.Models.RevenueCategory import RevenueCategory
 from FinancePlanning.Models.User import User
-from FinancePlanning.Repositories.Redudant.EarningRepository import EarningRepository
-from FinancePlanning.Repositories.Redudant.GoalRepository import GoalRepository
-from FinancePlanning.Repositories.Redudant.RevenueCategoryRepository import RevenueCategoryRepository
-from FinancePlanning.Repositories.Redudant.RevenueRepository import RevenueRepository
-from FinancePlanning.Repositories.Redudant.UserRepository import UserRepository
-
+from FinancePlanning.Repositories.XMLRepository import XMLRepository
+from Test.TestUtils import remove_test_repo
 
 class GoalServiceTest(unittest.TestCase):
 
+    PATH = "C:\\MyWindows\\Projects\\FinancePlanning\\FinancePlanning"
+
     def setUp(self):
-        revenues = RevenueRepository()
-        earnings = EarningRepository()
-        goals = GoalRepository()
+        self.revenues = XMLRepository("revenuesTest", self.PATH, Revenue)
+        self.earnings = XMLRepository("earningsTest", self.PATH, Earning)
+        self.goals = XMLRepository("goalsTest", self.PATH, Goal)
+        self.revenuesCategories = XMLRepository("revenuesCategoriesTest", self.PATH, RevenueCategory)
 
-        earnings.Add(Earning(0, 0, 1000000, EarningSource(0, "Salary"), datetime(2010, 1, 1)))
+        self.revenuesCategories.Add(RevenueCategory(0, "Salary"))
 
-        self.user = User(0, "Denis", "Belavin", earnings, revenues, goals)
+        self.earnings.Add(Earning(0, 0, 1000000, 0, str(datetime(2010, 1, 1))))
 
-        self.revenue_service = RevenuesService(RevenueRepository(), RevenueCategoryRepository())
+        self.user = User(0, "Denis", "Belavin")
 
-        self.user_repository = UserRepository()
-        self.user_repository.AddUser(self.user)
+        self.revenue_service = RevenuesService(self.revenues, self.revenuesCategories)
 
-        self.goal_service = GoalService(goals, self.user_repository, self.revenue_service)
+        self.user_repository = XMLRepository("user", self.PATH, User)
+        self.user_repository.Add(self.user)
+
+        self.goal_service = GoalService(self.goals, self.user_repository, self.earnings, self.revenues,
+                                        self.revenue_service)
 
     def test_create_goal_successfully(self):
         repository = self.goal_service.goal_repository
@@ -49,12 +54,20 @@ class GoalServiceTest(unittest.TestCase):
 
     def test_close_goal_successfully(self):
         self.goal_service.create_goal(self.user, "Car", 10000)
-        self.goal_service.close_goal(self.user.get_user_goals()[0])
+        self.goal_service.close_goal(self.user.get_user_goals(self.goals)[0])
 
-        self.assertTrue(self.user.get_user_goals()[0].closed)
+        self.assertTrue(self.user.get_user_goals(self.goals)[0].closed)
 
     def test_close_goal_unsuccessfully(self):
         self.goal_service.create_goal(self.user, "Car", 10000000)
-        self.goal_service.close_goal(self.user.get_user_goals()[0])
+        self.goal_service.close_goal(self.user.get_user_goals(self.goals)[0])
 
-        self.assertFalse(self.user.get_user_goals()[0].closed)
+        self.assertFalse(self.user.get_user_goals(self.goals)[0].closed)
+
+    def tearDown(self):
+        remove_test_repo(self.goals)
+        remove_test_repo(self.revenues)
+        remove_test_repo(self.revenuesCategories)
+        remove_test_repo(self.earnings)
+        remove_test_repo(self.user_repository)
+
--- Test/RevenueServiceTest.py
+++ Test/RevenueServiceTest.py
@@ -2,56 +2,66 @@ import unittest
 from datetime import datetime
 
 from FinancePlanning.Logic.RevenuesService import RevenuesService
+from FinancePlanning.Models.Earning import Earning
+from FinancePlanning.Models.Revenue import Revenue
+from FinancePlanning.Models.RevenueCategory import RevenueCategory
 from FinancePlanning.Models.User import User
-from FinancePlanning.Repositories.Redudant.EarningRepository import EarningRepository
-from FinancePlanning.Repositories.Redudant.RevenueCategoryRepository import RevenueCategoryRepository
-from FinancePlanning.Repositories.Redudant.RevenueRepository import RevenueRepository
+from FinancePlanning.Repositories.XMLRepository import XMLRepository
+from Test.GoalServiceTest import remove_test_repo
 
 
 class RevenueServiceTest(unittest.TestCase):
 
+    PATH = "C:\\MyWindows\\Projects\\FinancePlanning\\FinancePlanning"
+
     def setUp(self):
-        revenues = RevenueRepository()
-        earnings = EarningRepository()
+        self.revenues = XMLRepository("revenuesTest", self.PATH, Revenue)
+        self.earnings = XMLRepository("earningsTest", self.PATH, Earning)
+        self.revenues_categories = XMLRepository("revenuesCategoriesTest", self.PATH, RevenueCategory)
 
-        self.user = User(0, "Denis", "Belavin", earnings, revenues)
+        self.user = User(0, "Denis", "Belavin")
 
-        self.revenue_service = RevenuesService(revenues, RevenueCategoryRepository())
+        self.revenue_service = RevenuesService(self.revenues, self.revenues_categories)
 
     def test_create_revenue_successfully(self):
 
-        repository = self.revenue_service.revenue_repository
-        revenues_count = repository.GetAll().count()
+        revenues_count = len(self.revenues.GetAll())
 
         self.revenue_service.create_revenue(self.user, 100,
-                                            self.revenue_service.get_revenue_category_by_name("Shop"), datetime.now())
+                                            self.revenue_service.get_revenue_category_by_name("Shop").object_id,
+                                            datetime.now())
 
-        isRevenueAdded = repository.GetAll().count() == revenues_count + 1
+        isRevenueAdded = len(self.revenues.GetAll()) == revenues_count + 1
 
         self.assertTrue(isRevenueAdded)
 
     def test_create_revenue_negative_price(self):
 
-        repository = self.revenue_service.revenue_repository
-        revenues_count = repository.GetAll().count()
+        revenues_count = len(self.revenues.GetAll())
 
         self.revenue_service.create_revenue(self.user, -100,
-                                            self.revenue_service.get_revenue_category_by_name("Shop"), datetime.now())
+                                            self.revenue_service.get_revenue_category_by_name("Shop").object_id,
+                                            datetime.now())
 
-        isRevenueAdded = repository.GetAll().count() == revenues_count + 1
+        isRevenueAdded = len(self.revenues.GetAll()) == revenues_count + 1
 
         self.assertFalse(isRevenueAdded)
 
     def test_create_revenue_future_date(self):
 
-        repository = self.revenue_service.revenue_repository
-        revenues_count = repository.GetAll().count()
+        revenues_count = len(self.revenues.GetAll())
 
         date = datetime(2050, 1, 1)
 
         self.revenue_service.create_revenue(self.user, 100,
-                                            self.revenue_service.get_revenue_category_by_name("Shop"), date)
+                                            self.revenue_service.get_revenue_category_by_name("Shop").object_id,
+                                            date)
 
-        isRevenueAdded = repository.GetAll().count() == revenues_count + 1
+        isRevenueAdded = len(self.revenues.GetAll()) == revenues_count + 1
 
         self.assertFalse(isRevenueAdded)
+
+    def tearDown(self):
+        remove_test_repo(self.revenues)
+        remove_test_repo(self.earnings)
+        remove_test_repo(self.revenues_categories)
